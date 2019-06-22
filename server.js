87308e8e9573e8115ab71d38c865161d0b6cf159
Miguel Rodriguez <miguelantonio_rodriguez@ucol.mx>
2019-06-22 00:17:04 -0500

    modified for production

--- server.js
+++ server.js
@@ -6,11 +6,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/{{your-app-name}}'));
+app.use(express.static('./dist/need'));
 
 app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname,'/dist/need/index.html'));
+    res.sendFile(path.join(__dirname,'/dist/need/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
