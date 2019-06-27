35622be39db0a2357ed196a0d0f7786923ae8d80
Miguel Rodriguez <miguelantonio_rodriguez@ucol.mx>
2019-06-27 17:41:41 -0500

    key added to prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true, 
+  firebaseConfig: {
+    apiKey: "AIzaSyD0XTabizVpvEdD5rUacP_OvwvnabwYjsA",
+    authDomain: "need-f6bad.firebaseapp.com",
+    databaseURL: "https://need-f6bad.firebaseio.com",
+    projectId: "need-f6bad",
+    storageBucket: "need-f6bad.appspot.com",
+    messagingSenderId: "837988614895",
+    appId: "1:837988614895:web:30f9bc23694a8675"
+  }
 };
